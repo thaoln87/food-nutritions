fcf4c94d8bc62cffe4a33326d5fe1f38a6023de2
Le Ngoc Thao <thaoln87@gmail.com>
2017-03-22 11:25:55 +0700

    Update Food model and migration by setting some nullable field and default value

--- app/Food.php
+++ app/Food.php
@@ -4,6 +4,69 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Food
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $brand_id
+ * @property string $description
+ * @property string $image
+ * @property string $serving
+ * @property int $serving_per_container
+ * @property int $calories
+ * @property int $total_fat
+ * @property int $saturated
+ * @property int $polyunsaturated
+ * @property int $monounsaturated
+ * @property int $trans
+ * @property int $cholesterol
+ * @property int $sodium
+ * @property int $potassium
+ * @property int $total_carbs
+ * @property int $dietary_fiber
+ * @property int $sugar
+ * @property int $protein
+ * @property int $vitamin_a
+ * @property int $vitamin_c
+ * @property int $calcium
+ * @property int $iron
+ * @property bool $guaranteed
+ * @property bool $published
+ * @property bool $enabled
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereBrandId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereCalcium($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereCalories($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereCholesterol($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereDescription($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereDietaryFiber($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereEnabled($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereGuaranteed($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereImage($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereIron($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereMonounsaturated($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food wherePolyunsaturated($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food wherePotassium($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereProtein($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food wherePublished($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereSaturated($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereServing($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereServingPerContainer($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereSodium($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereSugar($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereTotalCarbs($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereTotalFat($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereTrans($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereVitaminA($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Food whereVitaminC($value)
+ * @mixin \Eloquent
+ */
 class Food extends Model
 {
     //
---
+++ database/migrations/2017_03_22_042321_create_foods_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('brand_id')->nullable()->unsigned();
+            $table->foreign('brand_id')->references('id')->on('brands');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->string('serving');
+            $table->integer('serving_per_container');
+            $table->integer('calories')->default(0);
+            $table->integer('total_fat')->default(0);
+            $table->integer('saturated')->default(0);
+            $table->integer('polyunsaturated')->default(0);
+            $table->integer('monounsaturated')->default(0);
+            $table->integer('trans')->default(0);
+            $table->integer('cholesterol')->default(0);
+            $table->integer('sodium')->default(0);
+            $table->integer('potassium')->default(0);
+            $table->integer('total_carbs')->default(0);
+            $table->integer('dietary_fiber')->default(0);
+            $table->integer('sugar')->default(0);
+            $table->integer('protein')->default(0);
+            $table->integer('vitamin_a')->default(0);
+            $table->integer('vitamin_c')->default(0);
+            $table->integer('calcium')->default(0);
+            $table->integer('iron')->default(0);
+            $table->boolean('guaranteed')->default(false);
+            $table->boolean('published')->default(false);
+            $table->boolean('enabled')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('foods');
+    }
+}
