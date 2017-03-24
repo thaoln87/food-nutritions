82da012da978c0b5956f85d05369b874e9225a27
Pham Ngoc Bao <pnbao@tma.com.vn>
2017-03-24 18:07:57 +0700

    Add test for FoodRequest validation

--- app/Http/Requests/FoodRequest.php
+++ app/Http/Requests/FoodRequest.php
@@ -24,7 +24,26 @@ class FoodRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => ['required', 'string', 'min:1']
+            'name' => ['required', 'string', 'min:1'],
+            'serving' => ['required', 'string', 'regex:/^((\d+\s+)(\d+\/\d+\s)|(\d+\/\d+\s)|(\d+\s+))[\w|\s]+$/'], // fraction text: 1/2 cup
+            'serving_per_container' => ['required', 'min:0', 'numeric'],
+            'calories'=> ['numeric', 'min:0'],
+            'total_fat'=> ['numeric', 'min:0'],
+            'saturated'=> ['numeric', 'min:0'],
+            'polyunsaturated'=> ['numeric', 'min:0'],
+            'monounsaturated'=> ['numeric', 'min:0'],
+            'trans'=> ['numeric', 'min:0'],
+            'cholesterol'=> ['numeric', 'min:0'],
+            'sodium'=> ['numeric', 'min:0'],
+            'potassium'=> ['numeric', 'min:0'],
+            'total_carbs'=> ['numeric', 'min:0'],
+            'dietary_fiber'=> ['numeric', 'min:0'],
+            'sugar'=> ['numeric', 'min:0'],
+            'protein'=> ['numeric', 'min:0'],
+            'vitamin_a'=> ['numeric', 'min:0'],
+            'vitamin_c'=> ['numeric', 'min:0'],
+            'calcium'=> ['numeric', 'min:0'],
+            'iron'=> ['numeric', 'min:0'],
         ];
     }
 }
--- tests/Http/Controllers/FoodController/FoodControllerValidationTest.php
+++ tests/Http/Controllers/FoodController/FoodControllerValidationTest.php
@@ -10,11 +10,42 @@ class FoodControllerValidationTest extends \Tests\TestCase
 {
     /**
      * @test
+     * @dataProvider getInvalidFoodData
      */
-    public function store_Empty_Name_Redirect_The_Previous_Page()
+    public function store_Invalid_Input_Redirect_To_Previous_Page($foodData, $testName)
     {
-        $response = $this->post('food', FoodDataBuilder::createFoodData(['name' => '']));
+        $response = $this->post('food', $foodData);
 
-        $response->assertStatus(302);
+
+        $this->assertEquals(302, $response->baseResponse->getStatusCode(), 'Fail: '.$testName);
+    }
+
+    public function getInvalidFoodData()
+    {
+        return array(
+            array(FoodDataBuilder::createFoodData(['name' => '']), 'name is empty'),
+            array(FoodDataBuilder::createFoodData(['serving_per_container' => -5]), 'serving_per_container is negative number'),
+            array(FoodDataBuilder::createFoodData(['calories' => -5]), 'calories is negative number'),
+            array(FoodDataBuilder::createFoodData(['total_fat' => -5]), 'total_fat is negative number'),
+            array(FoodDataBuilder::createFoodData(['saturated' => -5]), 'saturated is negative number'),
+            array(FoodDataBuilder::createFoodData(['polyunsaturated' => -5]), 'polyunsaturated is negative number'),
+            array(FoodDataBuilder::createFoodData(['monounsaturated' => -5]), 'monounsaturated is negative number'),
+            array(FoodDataBuilder::createFoodData(['trans' => -5]), 'trans is negative number'),
+            array(FoodDataBuilder::createFoodData(['cholesterol' => -5]), 'cholesterol is negative number'),
+            array(FoodDataBuilder::createFoodData(['sodium' => -5]), 'sodium is negative number'),
+            array(FoodDataBuilder::createFoodData(['potassium' => -5]), 'potassium is negative number'),
+            array(FoodDataBuilder::createFoodData(['total_carbs' => -5]), 'total_carbs is negative number'),
+            array(FoodDataBuilder::createFoodData(['cholesterol' => -5]), 'cholesterol is negative number'),
+            array(FoodDataBuilder::createFoodData(['dietary_fiber' => -5]), 'dietary_fiber is negative number'),
+            array(FoodDataBuilder::createFoodData(['sugar' => -5]), 'sugar is negative number'),
+            array(FoodDataBuilder::createFoodData(['protein' => -5]), 'protein is negative number'),
+            array(FoodDataBuilder::createFoodData(['vitamin_a' => -5]), 'vitamin_a is negative number'),
+            array(FoodDataBuilder::createFoodData(['vitamin_c' => -5]), 'vitamin_c is negative number'),
+            array(FoodDataBuilder::createFoodData(['calcium' => -5]), 'calcium is negative number'),
+            array(FoodDataBuilder::createFoodData(['iron' => -5]), 'iron is negative number'),
+            array(FoodDataBuilder::createFoodData(['serving' => 'a']), 'Serving does not begin with a number'),
+            array(FoodDataBuilder::createFoodData(['serving' => '10']), 'Serving does not end with a text'),
+            array(FoodDataBuilder::createFoodData(['serving' => '10.5 cup']), 'Serving does not use decimal'),
+        );
     }
 }
